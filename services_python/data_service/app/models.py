b2b8e8f382b3714180a4e6950db71d1701d596cd
Tran Hung <tranviethung912002@gmail.com>
2024-03-02 19:42:08 +0700

    update model relationship

--- services_python/data_service/app/models.py
+++ services_python/data_service/app/models.py
@@ -3,50 +3,75 @@ import sys
 sys.path.append(".")
 
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String, JSON, DateTime, func, text
 from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
+import sqlalchemy as db
 
 
 class Base(declarative_base()):
     __abstract__ = True
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
-    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+    created_at = db.Column(db.DateTime(timezone=True), server_default=db.func.now())
+    updated_at = db.Column(db.DateTime(timezone=True), onupdate=db.func.now())
 
 
 class Datasource(Base):
     __tablename__ = "datasources"
 
-    id = Column(
-        UUID(as_uuid=True), primary_key=True, server_default=text("uuid_generate_v4()")
+    id = db.Column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        server_default=db.text("uuid_generate_v4()"),
     )
-    user_id = Column(UUID(as_uuid=True), server_default=text("uuid_generate_v4()"))
-    name = Column(String)
-    type = Column(String)
-    host = Column(String)
-    port = Column(String)
-    other = Column(JSON)
+    user_id = db.Column(
+        UUID(as_uuid=True), server_default=db.text("uuid_generate_v4()")
+    )
+    name = db.Column(db.String, nullable=False)
+    type = db.Column(db.String, nullable=False)
+    host = db.Column(db.String, nullable=False)
+    port = db.Column(db.String, nullable=False)
+    other = db.Column(db.JSON)
+    # Định nghĩa mối quan hệ một nhiều với bảng Dataset
+    datasets = relationship("Dataset", back_populates="datasource")
 
 
 class Dataset(Base):
     __tablename__ = "datasets"
 
-    id = Column(
-        UUID(as_uuid=True), primary_key=True, server_default=text("uuid_generate_v4()")
+    id = db.Column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        server_default=db.text("uuid_generate_v4()"),
     )
-    datasource_id = Column(
-        UUID(as_uuid=True), server_default=text("uuid_generate_v4()")
+    datasource_id = db.Column(
+        UUID(as_uuid=True),
+        db.ForeignKey("datasources.id"),
+        nullable=False,
+        server_default=db.text("uuid_generate_v4()"),
     )
-    name = Column(String)
-    other = Column(JSON)
+    name = db.Column(db.String, nullable=False)
+    other = db.Column(db.JSON)
+    # Định nghĩa mối quan hệ nhiều một với bảng Datasource
+    datasource = relationship("Datasource", back_populates="datasets")
+    # Định nghĩa mối quan hệ một nhiều với bảng DatasetVersion
+    dataset_versions = relationship("DatasetVersion", back_populates="dataset")
 
 
 class DatasetVersion(Base):
     __tablename__ = "dataset_versions"
 
-    id = Column(
-        UUID(as_uuid=True), primary_key=True, server_default=text("uuid_generate_v4()")
+    id = db.Column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        server_default=db.text("uuid_generate_v4()"),
     )
-    dataset_id = Column(UUID(as_uuid=True), server_default=text("uuid_generate_v4()"))
-    name = Column(String)
-    schema = Column(JSON)
-    other = Column(JSON)
+    dataset_id = db.Column(
+        UUID(as_uuid=True),
+        db.ForeignKey("datasets.id"),
+        nullable=False,
+        server_default=db.text("uuid_generate_v4()"),
+    )
+    name = db.Column(db.String, nullable=False)
+    schema = db.Column(db.JSON)
+    other = db.Column(db.JSON)
+    # Định nghĩa mối quan hệ nhiều một với bảng Dataset
+    dataset = relationship("Dataset", back_populates="dataset_versions")
