5fd6e279ccdc131774e88f838ed81c4fbe1ef0ec
hungdaqq <hungdaqq@gmail.com>
2024-06-06 11:39:53 +0700

    Update APIRouter

--- services_python/ml_service/app/api/__init__.py
+++ services_python/ml_service/app/api/__init__.py
@@ -2,8 +2,9 @@ from fastapi import APIRouter
 
 from .projects import router as project_router
 from .runs import router as run_router
+from .ml_models import router as model_router
 
-
-router = APIRouter(prefix="/api/ml")
+router = APIRouter(prefix="/api/mlops", tags=["MLOps API"])
 router.include_router(project_router)
 router.include_router(run_router)
+router.include_router(model_router)
