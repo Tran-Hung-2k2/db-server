2eacce6812d450002e2184e6e1260dce70a791cc
hungdaqq <hungdaqq@gmail.com>
2024-06-10 16:57:21 +0700

    Update S3 utils

--- services_python/utils/s3.py
+++ services_python/utils/s3.py
@@ -4,12 +4,12 @@ import boto3
 # from botocore.client import Config
 
 
-MINIO_ACCESS_KEY_ID = os.getenv("MINIO_ACCESS_KEY_ID")
-MINIO_SECRET_ACCESS_KEY = os.getenv("MINIO_SECRET_ACCESS_KEY")
-MINIO_ENDPOINT_URL = os.getenv("MINIO_ENDPOINT_URL")
-MINIO_ALLOW_HTTP = os.getenv("MINIO_ALLOW_HTTP", "true")
-MINIO_REGION = os.getenv("MINIO_REGION", "us-east-1")
-MINIO_S3_ALLOW_UNSAFE_RENAME = os.getenv("MINIO_S3_ALLOW_UNSAFE_RENAME", "true")
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_ENDPOINT_URL = os.getenv("AWS_ENDPOINT_URL")
+# MINIO_ALLOW_HTTP = os.getenv("MINIO_ALLOW_HTTP", "true")
+# MINIO_REGION = os.getenv("MINIO_REGION", "us-east-1")
+# MINIO_S3_ALLOW_UNSAFE_RENAME = os.getenv("MINIO_S3_ALLOW_UNSAFE_RENAME", "true")
 
 
 def save_to_s3(bucket, entry, content):
@@ -17,9 +17,9 @@ def save_to_s3(bucket, entry, content):
     # Set up MinIO client
     s3 = boto3.resource(
         "s3",
-        endpoint_url=MINIO_ENDPOINT_URL,
-        aws_access_key_id=MINIO_ACCESS_KEY_ID,
-        aws_secret_access_key=MINIO_SECRET_ACCESS_KEY,
+        endpoint_url=AWS_ENDPOINT_URL,
+        aws_access_key_id=AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
         # config=Config(signature_version="s3v4"),
     )
     # Create the bucket if it doesn't exist
